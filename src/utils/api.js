77dc0ddc3617d077151dd63820de70a365b19c0f
logical-mindsit-web <logicalmindsit.careers@gmail.com>
2024-12-30 12:26:02 +0530

    Live link Changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',  
+  baseURL: 'https://admin-website-backend.onrender.com',  
  // baseURL: '',  
 });
 api.interceptors.request.use(config => {
