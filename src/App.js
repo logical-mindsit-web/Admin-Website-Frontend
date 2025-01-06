a5ee053488a4ff805e90c8646cb42c200e051354
logical-mindsit-web <logicalmindsit.careers@gmail.com>
2025-01-06 16:12:00 +0530

    refress issue

--- src/App.js
+++ src/App.js
@@ -86,11 +86,67 @@
 // // export default App;
 
 
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
+// import Header from './HomePage/Header';
+// import AfterLoginRoutes from "./Routes/AfterLoginRoutes "
+// import Footer from "./HomePage/Fooder";
+// const App = () => {
+//   const isAuthenticated = () => {
+//     const token = localStorage.getItem('token');
+//     return !!token;
+//   };
+
+//   const AppWrapper = () => {
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//       if (isAuthenticated()) {
+//         navigate('/card');
+//       } else {
+//         navigate('/');
+//       }
+//     }, []);
+
+//     return (
+//       <div style={styles.app}>
+//         {isAuthenticated() && <Header />}
+//         <div style={styles.content}>
+//           <AfterLoginRoutes />
+//         </div>
+//         {isAuthenticated() && <Footer />}
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <Router>
+//       <AppWrapper />
+//     </Router>
+//   );
+// };
+
+// const styles = {
+//   app: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     minHeight: '100vh',
+//   },
+//   content: {
+//     flex: 1,
+//     position: 'relative',
+//   },
+// };
+
+// export default App;
+
+
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
 import Header from './HomePage/Header';
 import AfterLoginRoutes from "./Routes/AfterLoginRoutes "
 import Footer from "./HomePage/Fooder";
+import { useLocation } from 'react-router-dom';
 const App = () => {
   const isAuthenticated = () => {
     const token = localStorage.getItem('token');
@@ -98,15 +154,23 @@ const App = () => {
   };
 
   const AppWrapper = () => {
-    const navigate = useNavigate();
 
+    const navigate = useNavigate();
+    const location = useLocation();
+  
     useEffect(() => {
+      const path = location.pathname;
+  
       if (isAuthenticated()) {
-        navigate('/card'); 
+        if (path === '/') {
+          navigate('/card'); 
+        }
       } else {
-        navigate('/'); 
+        if (path !== '/') {
+          navigate('/'); 
+        }
       }
-    }, []); 
+    }, [location.pathname]);
 
     return (
       <div style={styles.app}>
